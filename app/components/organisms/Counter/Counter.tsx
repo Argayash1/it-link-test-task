a1c6a00fe171692a59ced366d4d906485517c413
Argayash <zilberman.yakoff@yandex.ru>
2024-05-31 19:40:15 +0300

    feat: вынести логику из Counter в кастомный хук useCounter

--- app/components/organisms/Counter/Counter.tsx
+++ app/components/organisms/Counter/Counter.tsx
@@ -4,15 +4,14 @@ import { ReactElement, useState } from 'react';
 import { Button, ButtonType } from '../../atoms/Button/Button';
 import styles from './Counter.module.css';
 import { ButtonText } from '../../molecules/ButtonText/ButtonText';
+import useCounter from '@/app/hooks/useCounter';
 
 export const Counter = (): ReactElement => {
-  const [index, setIndex] = useState<number>(0);
-  const increment = () => setIndex(index + 1);
-  const decrement = () => setIndex(index - 1);
+  const { count, increment, decrement } = useCounter();
 
   return (
     <div className={styles.counter}>
-      <ButtonText text={index} buttonText='increment' onClick={increment} buttonType={ButtonType.BUTTON} />
+      <ButtonText text={count} buttonText='increment' onClick={increment} buttonType={ButtonType.BUTTON} />
       <Button onClick={decrement} buttonText='decrement' buttonType={ButtonType.BUTTON} />
     </div>
   );
